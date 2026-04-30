f0da61ed6a2e123110ea38c7b99b4936662432a6
Gabriel Torres <gabrielstorres08@gmail.com>
2026-04-30 15:15:59 -0300

    add route spoc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import WhatsAppMassivos from './pages/WhatsAppMassivos';
 import ScriptsSD from './pages/ScriptsSD';
 import AccessControl from './pages/AccessControl';
 import ProcessamentoMassa from './pages/ProcessamentoMassa';
+import MesaSpoc from './pages/MesaSpoc';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/scripts-sd" element={<ScriptsSD />} />
         <Route path="/access-control" element={<AccessControl />} />
         <Route path="/processamento-massa" element={<ProcessamentoMassa />} />
+        <Route path="/mesa-spoc" element={<MesaSpoc />} />
       </Routes>
     </BrowserRouter>
   );
